6fceed2762e2e8629e8dac8772f006796a26f508
Massimo <massimo.taddi02@gmail.com>
2023-11-12 20:01:41 +0100

    Variables

---
+++ C#/Variables/Array/Program.cs
@@ -0,0 +1,119 @@
+﻿/*
+ what is a array?
+ first of let's think of the array as a multiple containers in one variable.
+ 
+ the basic syntax would be:
+
+ value[] name = new int [length of the array];
+
+ or you can also initialize the array with already some values in it
+
+ int[] arrayExample= {1,2,3,4,5};
+
+ with this we initialize an array that in his position 0 has the value 1
+
+ int[] arrayExample1= new int [5];
+
+ we are now initializing an array that has 5 empty spaces inside it
+
+ to help you understand it better this is what an array theoretically looks like
+ 
+ position (index) on the arrayExample:
+                  0       1       2      3        4
+              ________ _______  ______ _______  ______
+array value   |   1  | |  2  |  | 3  | | 4  |   | 5  |
+              ________ _______  ______ _______  ______
+ 
+ */
+
+int[] arrayExample = new int[5];
+int[] arrayExample1 = {1,2,3,4,5};
+
+
+
+/*
+ * if we want to assing a specific value to a specific place in the array this is the synstax
+ */
+arrayExample[0] = 5;
+Console.WriteLine(arrayExample[0]);
+
+Console.WriteLine("write a value");
+int value=int.Parse(Console.ReadLine());
+arrayExample[1] = value;
+Console.WriteLine(arrayExample[1]);
+
+//IMPORTANT THINGS TO KNOW:
+/*
+ * 
+ * you cant print an array without telling it wich position you want to print
+ * if you want to try print an array without the position you are going to receive a  
+ * "System.Int32[]" on the console which means that 
+ * the console recive an array to print but he does not know the position you want to print
+ 
+ * Console.WriteLine(arrayExample); <---WRONG!
+ * Console.WriteLine(arrayExample[0]); <----Right!
+
+ * Array can be initialize as whatever type of varible you like it can be an array of Objects, int, string ect.
+
+ * another important thing to know is that his length MUST be defined when we initialize it 
+ * unlike the list that we are gonna see soon.
+ */
+
+
+
+
+//Lists
+/*
+ * Lists are very much similar to the arrays 
+ * the main difference to always keep in mind is that 
+ * when you initialize a list you dont have to specify his lenght
+ */
+List<int> intList = new List<int>();
+
+// to assign a value you must write listname.add();
+intList.Add(50);
+intList.Add(900);
+
+// good to know
+/*
+ * With lists there exists some methods that can be useful:
+ * .Count it count the elements in the list
+ * .RemoveAt() to delete the value of a specific index
+ * .Remove() to delete a specified value present in the list
+ */
+
+//Dictionary 
+/*
+ * Dictionary are not exacly a kind of variable that work with indexes but they work 
+ * in a very similar method 
+ */
+
+
+//initialize a dictioanary 
+
+Dictionary<string,string> dictionaryExample = new Dictionary<string,string>();
+
+/*
+ * the main particularity of dictionaries is that this kind of variable get 2 type of
+ * variable when you initialize it in this case <string,string>
+ * the first string is called "key" and the second string is called "value"
+ * the "key" has a very similar role to the indexes in arrays and lists but in this case
+ * it can be a variable as well:
+ * you can initlialize a Dictionary with different kinds of variables 
+ * not only string string
+ */
+
+//basic funtionalities
+
+dictionaryExample["a"] = "hello";
+//we are telling to add a container inside de dictionary
+//with the key "a" and the value inside that container to be "hello"
+Console.WriteLine(dictionaryExample["a"]);
+//On the program the result will be the value attached to the key named "a"
+
+//another example
+
+Dictionary<string,int> nameAndAge=new Dictionary<string,int>();
+
+nameAndAge.Add("hello", 70);
+Console.WriteLine(nameAndAge["hello"]);
---
+++ C#/Variables/Generic Variables/Program.cs
@@ -0,0 +1,127 @@
+﻿/*
+ * let's think of variables as "containers" the synstax for the most "basic" variables is  
+ * "type name=value"
+ * for example
+ */
+//int stands for integer and is use to assign a numeric value without comma.
+// 5 is a int type of value.
+// 5.1 is a double type of value
+
+int value0 = 5;
+
+//to give a variable a value with a comma we use double
+double value1 = 6.2;
+
+//if we want to save text or lines of text we save it as string
+string phrase = "hello world";
+
+//bolean is a type of value that can only be true or false we will learn out to use it
+bool response = true;
+
+/*
+ * those are not every varible that can be used,
+ * there exist many, many more "basic" variables.
+ * those are just the ones that we are gonna use
+ */
+
+/*
+ * Console commands:
+ * for this tutorial we are going to use a couple of console command 
+ * Console.WriteLine() allows you to write anything you want on the console while running the program
+ * Console.ReadLine() allows you to give a input to the console while running.
+ */
+
+
+Console.WriteLine("hi");
+// you can print on console the value of the variables just buy putting their variable name insinde the () of Console.WriteLine()
+Console.WriteLine((value0) + phrase);
+
+//Console.ReadLine() allows you to recive a input from the console
+
+Console.ReadLine();
+
+// you can use read line to give value to a variable for example
+
+string word = Console.ReadLine();
+Console.WriteLine("this is the word you wrote " + word);
+
+
+/* 
+* by default Console.ReadLine() will expect you to write a string but if you want to 
+* assign a value of different type you must parse it.
+* 
+* you can parse it by simply putting:
+* 
+* variabletype.Parse(valuetocheck) a easy example would be
+* 
+* int answer=int.Parse(Console.ReadLine());
+* 
+* in the example we initialize a int type variable and we make it equals
+* to the input that we receive in the console converted as int.
+* if the console find strings while he tries to parse it the program will break.
+* 
+*/
+
+
+
+//BASIC OPERATIONS
+/*
+ * the most basic operation that we can have are
+ * 
+ * Addiction wich uses + sign
+ * Subtraction wich uses - sign
+ * Multiplication wich uses * sign
+ * Division which uses + sign
+ * 
+ * Examples
+ * 
+ */
+//example for int value.
+
+//Addiction
+/* 
+int valueToSum1 = 1;
+int valueToSum2 = 2;
+
+int result = valueToSum1 + valueToSum2;
+Console.WriteLine("Result of the Addition"+result);
+*/
+//Subtraction 
+/* 
+int higherValue = 5;
+int valueToSubtract = 3;
+int result1 = higherValue - valueToSubtract;
+Console.WriteLine("Result of the Subtraction "+result1);
+*/
+//Multiplication 
+/* 
+int value2 = 2;
+int value3 = 2;
+int result2 = value2 * value3;
+Console.WriteLine("Result of the Multiplication " + result2);
+*/
+//Division 
+/* 
+int Value4 = 6;
+int value5 = 3;
+int result3 = Value4 / value5;
+Console.WriteLine("Result of the Division " + result3);
+*/
+
+//example for string value
+/*
+ * beign a string dosent allow you to do most of the operations but you can concatenate more strings
+ */
+
+string firstName = "Mike";
+string lastName = "Tyson";
+Console.WriteLine(firstName + " " + lastName);
+
+//good to know 
+/*
+ * be careful when you initialize something, you can save values as string for example
+ * 1 can be saved as a string but it will also be treated as such 
+ * if you initialize a value of type string and you insert a numeric value 
+ * the program will treat that string as text and not as a int value, and for that you cannot
+ * use operations as + - * / because the console treat it as a string and not int
+ */
